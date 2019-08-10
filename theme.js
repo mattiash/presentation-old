9240a0afbd2ad8a83927c7dfddf314f51d26f7ae
Mattias Holmlund <mattias.holmlund@netinsight.net>
2019-08-10 12:19:42 +0200

    Upgrade mdx-deck to 3.0.9

--- theme.js
+++ theme.js
@@ -1,11 +1,12 @@
-export default {
-    css: {
-        paddingLeft: "1em",        
-        paddingRight: "1em",        
-    },
-
-    li: {
-            marginTop: '0.7em',
-            lineHeight: '1.3'
+export const theme = {
+    styles: {
+        h1: {
+            textTransform: 'uppercase',
+            letterSpacing: '0.1em',
+            color: blue
+        },
+        blockquote: {
+            fontStyle: 'italic',
+        },
     }
 }
