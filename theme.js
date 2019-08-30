f2e19b8dc6804dcab93477f49cac5494e189958f
Mattias Holmlund <mattias.holmlund@netinsight.net>
2019-08-30 09:37:18 +0200

    NI Theming wip

--- theme.js
+++ theme.js
@@ -1,11 +1,38 @@
+export const ni_yellow = '#ffd800'
+export const ni_red = '#fa3c5a'
+export const ni_blue = '#0055ff'
+export const ni_green = '#0ae6be'
+
+export const ni_light_yellow = '#fff3b2'
+export const ni_light_red = '#fdc5ce'
+export const ni_light_blue = '#b2ccff'
+export const ni_light_green = '#b6f8ec'
+
+export const ni_dark_yellow = '#dc8c28'
+export const ni_dark_red = '#96281e'
+export const ni_dark_blue = '#001eaa'
+export const ni_dark_green = '#0a6e78'
+
 export const theme = {
+    googleFont: 'https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab|Roboto+Mono',
+    fonts: {
+        body: 'Roboto'
+    },
     styles: {
         h1: {
-            textTransform: 'uppercase',
-            letterSpacing: '0.1em',
-            color: blue
+            fontFamily: 'Roboto Slab',
+            wordSpacing: '-10%',
+            weight: 'bold',
+            color: ni_blue,
+        },
+        h2: {
+            fontFamily: 'Roboto',
+            weight: 'bold',
+            lineHeight: '110%'
         },
         blockquote: {
+            fontFamily: 'Roboto Mono',
+            lineHeight: '120%',
             fontStyle: 'italic',
         },
     }
